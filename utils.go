6533e0da55e090f56c1b57e896b17a1df43c1fd6
rei1011 <r.mizobuchi3@gmail.com>
2023-05-05 00:29:07 +0000

    便利関数を切り出した

---
+++ utils.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"strconv"
+	"strings"
+)
+
+// スペース区切りで入力をintのスライスに変換する
+func toIntArray() []int {
+	var slice []int
+	sc.Scan()
+	x := strings.Split(sc.Text(), " ")
+	for _, input := range x {
+		num, _ := strconv.Atoi(input)
+		slice = append(slice, num)
+	}
+	return slice
+}
+
+// スペース区切りで入力をfloat64のスライスに変換する
+func toFloat64Array() []float64 {
+	var slice []float64
+	sc.Scan()
+	x := strings.Split(sc.Text(), " ")
+	for _, input := range x {
+		num, _ := strconv.ParseFloat(input, 64)
+		slice = append(slice, num)
+	}
+	return slice
+}
+
+func toStringArray() []string {
+	return strings.Split(sc.Text(), " ")
+}
+
+// 空白もしくは改行ごとにinputを読み込むための設定
+func scanWords() {
+	sc.Split(bufio.ScanWords)
+}
+
+// stringをintに変換する
+func toInt(input string) int {
+	num, _ := strconv.Atoi(input)
+	return num
+}
+
+// intの配列を作成
+func makeIntArray(length int) []int {
+	return make([]int, length, length)
+}
